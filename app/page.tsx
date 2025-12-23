2a9ac8debb8e411a61c9d87e840e585f3087973d
ibnu ahmad yuswandana <113858558+ibnuahmd@users.noreply.github.com>
2025-12-23 13:11:14 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold fade-in">
+        Hello, Ibnu 👋
+      </h1>
+    </main>
+  )
+}
 
